4f720ea5aa147af751fdaacc8d56edd0eec3ab14
Eugene Millerman <e.v.millerman@gmail.com>
2013-12-20 16:55:29 -0500

    Finished problem 3, started problem 4

--- problem3.rb
+++ problem3.rb
@@ -12,22 +12,17 @@ def is_prime(n)
 	true
 end
 
-i = 1
-puts "#{i} is prime: #{is_prime(i)}"
-i = 7
-puts "#{i} is prime: #{is_prime(i)}"
-i = 9
-puts "#{i} is prime: #{is_prime(i)}"
-i = 12
-puts "#{i} is prime: #{is_prime(i)}"
-i = 13
-puts "#{i} is prime: #{is_prime(i)}"
-
-
-def largest_prime(n)
+def get_prime_factors(n)
+	return [n] if is_prime(n)
+	(2...n/2).each do |x|
+		if n%x ==0
+			return get_prime_factors(x).concat(get_prime_factors(n/x))
+		end
+	end
 end
 
-puts largest_prime(13195) == 29
+puts get_prime_factors(13195).max == 29
+puts get_prime_factors(600851475143).max
 
 
 
