cdbec101b4067e0ebff2cefd0cd7bd7df9bda239
Eugene Millerman <e.v.millerman@gmail.com>
2014-04-26 23:07:54 -0400

    Finish javascript001, init 002

--- javascript/problem001.js
+++ javascript/problem001.js
@@ -2,9 +2,9 @@
 // we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 // Find the sum of all the multiples of 3 or 5 below 1000.
-
+"use strict";
 var multiple_of = function (divisor) {
-  return function (dividend) {
+        return function (dividend) {
             return dividend % divisor === 0;
         };
     };
@@ -12,9 +12,9 @@ var multiple_of = function (divisor) {
 var multiple_of_3 = multiple_of(3);
 var multiple_of_5 = multiple_of(6);
 
-var sum = 0,i=0;
+var sum = 0, i = 0;
 
-for(i=0;i<=1000;i++){
+for(i = 0;i <= 1000;i++){
   if (multiple_of_3(i) || multiple_of_5(i)) {
     sum += i;
   }
